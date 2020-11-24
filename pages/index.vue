563c86e50f1e514d7bc72d0d5119beb9813fe1c6
hasan <45624754+hasanzarghami@users.noreply.github.com>
2020-11-24 11:57:38 +0330

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12 d-flex align-items-center justify-content-center vh-100">
+        <file-upload uploadUrl="/upload/file" uploadKey="uploadfile" :params="{params:1}"
+                     :files.sync="images"  :maxFileSize="2"
+                     fileType="image"></file-upload>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import fileUpload from "@/components/fileUpload";
+
+  export default {
+    components: {fileUpload},
+    data: () => ({
+      images: []
+    })
+
+  }
+</script>
+
+<style>
+
+</style>
